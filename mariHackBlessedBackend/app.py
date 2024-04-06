ad9d91493398eb6a55db95362e6d463690dcbdc5
ericlu0306 <ericlu3141592@gmail.com>
2024-04-06 05:51:02 -0400

    AAAAAAAAAAAAAAAABAAAAAHAHAHAHAAAAAAAAHAAAAAAAAh

--- mariHackBlessedBackend/app.py
+++ mariHackBlessedBackend/app.py
@@ -1,7 +1,9 @@
-from flask import Flask
-from flask_pymongo import PyMongo
+from json import dumps
+
+from flask import Flask, Response
+from flask_pymongo import PyMongo, MongoClient
 from apscheduler.schedulers.background import BackgroundScheduler
-import requests
+from bson.json_util import dumps
 
 from datetime import datetime, timedelta
 
@@ -9,15 +11,13 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/bankHack"
 mongo = PyMongo(app)
 
+
 def computeBrokerIndex():
-
-
-
     return 1
 
 
-
 incrementedTime = 0
+
 
 def fetch_data_and_store():
     global incrementedTime
@@ -30,15 +30,13 @@ def fetch_data_and_store():
     datetime_part = datetime.strptime(datetime_part_str, "%Y-%m-%d %H:%M:%S")
 
     incremented_datetime1 = datetime_part + timedelta(seconds=incrementedTime)
-    incremented_datetime2 = datetime_part + timedelta(seconds=incrementedTime+3)
+    incremented_datetime2 = datetime_part + timedelta(seconds=incrementedTime + 3)
 
     incremented_datetime_str1 = incremented_datetime1.strftime("%Y-%m-%d %H:%M:%S")
     incremented_datetime_str2 = incremented_datetime2.strftime("%Y-%m-%d %H:%M:%S")
 
     incremented_time_str1 = f"{incremented_datetime_str1}.{nanosecond_part_str}"
     incremented_time_str2 = f"{incremented_datetime_str2}.{nanosecond_part_str}"
-
-    print(incremented_time_str1, incremented_time_str2)
 
     data_cursor = mongo.db.bankHack_mock.find({
         "time": {"$gte": incremented_time_str1, "$lt": incremented_time_str2}
@@ -55,11 +53,6 @@ def fetch_data_and_store():
         # Insert documents into the target collection
         result = mongo.db.bankHackData.insert_many(data_list)
 
-        print(f"Transferred {len(result.inserted_ids)} documents to bankHackData.")
-    else:
-        print("No documents found to transfer.")
-
-
 
 @app.route('/getBrokerIndex')
 def getBrokerIndexById():
@@ -70,12 +63,22 @@ def getBrokerIndexById():
 def getTopBrokers():
     return 'Hello, World!'
 
+
 @app.route('/getRecentTrades')
 def getRecentTrades():
+    data_cursor = mongo.db.bankHack_mock.find({'Message Type': 'E'})
 
-    return 'Hello, World!'
+    # Convert the cursor to a list here, but this consumes the cursor
+    data_list = list(data_cursor)
 
+    # Print to console (for debugging purposes)
+    print(data_list)
 
+    # Convert list to JSON string
+    data_json = dumps(data_list)
+
+    # Return JSON response
+    return Response(data_json, mimetype='application/json')
 
 
 @app.route('/getBrokerIndexPlot')
