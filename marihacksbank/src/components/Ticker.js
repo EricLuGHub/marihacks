5732fb474612f634789c203a3b36e9f17fc1cb83
HalogenLightBlob <100003405+HalogenLightBlob@users.noreply.github.com>
2024-04-06 12:35:04 -0400

    updated ticker

--- marihacksbank/src/components/Ticker.js
+++ marihacksbank/src/components/Ticker.js
@@ -1,5 +1,17 @@
 import "../styles/Ticker.css"
 
+function randomStock() {
+  let value = Math.round((Math.random()*100 + 20) * 100);
+  let valueStr = value.toString();
+
+  let up = Math.random() > .5;
+  return <div up={up ? "true" : ""}>{valueStr.substring(0, valueStr.length-2)}.{valueStr.substring(valueStr.length-2)} {up ? "▲" : "▼"}</div>
+}
+
 export default function RankingHistory() {
-  return <div className="Ticker"><div>pineapple pizza</div><div>pineapple pizza</div><div>pineapple pizza</div><div>pineapple pizza</div><div>pineapple pizza</div><div>pineapple pizza</div></div>;
+  return <div className="Ticker">
+    <div className="TickerElementHolder">
+      {[...Array(100)].map(randomStock)}
+    </div>
+  </div>;
 }
