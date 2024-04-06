f2e4f95a6086cb4ccba87350bdbd3ad811666619
a-liubai <audrey11luxe@gmail.com>
2024-04-05 20:07:21 -0400

    changing port

--- mariHacksBackend/app.js
+++ mariHacksBackend/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hellooo World!');
 });
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
